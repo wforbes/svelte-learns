d4864d9e43efa7e531ab1bd353fb77cff1e4455e
Will Forbes <wforbes87@gmail.com>
2025-08-30 23:12:36 -0700

    svelte.dev tutorial: Dynamic attributes
    - you can use curly brace syntax in attribute values, thats cool
    - shorthand attribute, where 'src' variable name matches the 'src' attribute element property name, allows you to just curlybrace your variable directly into the element attribute list
    - image goes into the static folder

--- src/routes/first-tutorial/+page.svelte
+++ src/routes/first-tutorial/+page.svelte
@@ -1,9 +1,18 @@
 <script lang="ts">
 	let name = "Svelte";
+	let imgSubject = "Ross"
+	let src = "/code-meme.png"
 </script>
 
 <h1>Welcome</h1>
 
-<p class="text-center">
-	Hello {name.toUpperCase()}!
-</p>
+<div class="mx-auto">
+	<p>
+		Hello {name.toUpperCase()}!
+	</p>
+	<img
+		{src}
+		alt="{imgSubject} is sarcastically moody, 90s people loved that."
+		width="300"
+	/>
+</div>
